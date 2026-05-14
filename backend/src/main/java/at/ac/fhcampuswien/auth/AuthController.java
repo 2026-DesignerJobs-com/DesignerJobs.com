2ffa0e33fe16bcd07adca1c4aa59222a7f9871cb
Lika Kevlishvili <lika.kevlishvili@stud.hcw.ac.at>
2026-05-14 20:22:08 +0200

    Prepare login and register frontend and auth connection

--- backend/src/main/java/at/ac/fhcampuswien/auth/AuthController.java
+++ backend/src/main/java/at/ac/fhcampuswien/auth/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:63342", "http://localhost:63343"})
 @RequestMapping("/auth")
 public class AuthController {
 
